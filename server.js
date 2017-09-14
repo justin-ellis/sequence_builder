947239802887e0f85f4cca9b02ff5c5d43cf1ac3
Jellis <venetianself@hotmail.com>
2017-09-14 08:35:15 -0500

    angular is working

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 
