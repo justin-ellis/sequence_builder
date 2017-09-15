3131eff0a57719493ead5a94e3feee0683ae51e8
Jellis <venetianself@hotmail.com>
2017-09-15 11:34:52 -0500

    authentication functions set up, sessions installed

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,107 @@
 const app = angular.module('sequence-builder', []);
 
 app.controller('mainController', ['$http', function($http){
-	const controller = this;
 	console.log('angular is here');
+	const controller = this;
+	this.poses = [];
+	this.hideForm = true;
+	this.showLogin = false;
+	
+	this.displayLogin = function(){
+		this.showLogin = true;
+	};
+
+	this.hideLogin = function(){
+		this.concealLogin = true;
+	};
+
+	this.getYogaPoses = function(){
+		$http({
+			method: 'GET',
+			url: '/poses'
+		}).then(
+		function(response){
+			controller.poses = response.data;
+		},
+		function(err){
+			console.log(err);
+		});
+	};
+
+	this.registerUser = function(username, password){
+		$http({
+			method: 'POST',
+			url: '/session/register',
+			data: {
+				username: this.registeredUsername,
+				password: this.registeredPassword
+			}
+		}).then(
+		function(response){
+			controller.registeredUsername = '';
+			controller.registeredPassword = '';
+			controller.newUser = response.data;
+
+			if(response.data){
+				controller.loggedIn = true;
+				controller.hideForm = false;
+			}
+			controller.hideLogin();
+		},
+		function(err){
+			console.log(err);
+		});
+	};
+
+	this.login = function(username, password){
+		$http({
+			method: 'POST',
+			url: '/session/login',
+			data: {
+				username: this.username,
+				password: this.password
+			}
+		}).then(
+		function(response){
+			controller.username = '';
+			controller.password = '';
+			controller.foundUser = response.data;
+			console.log(response.data);
+
+			if(response.data){
+				controller.loggedIn = true;
+				controller.hideForm = false;
+			}
+			controller.hideLogin();
+		},
+		function(err){
+			console.log(err);
+		});
+	};
+
+
+
+	this.logout = function(username, password){
+		$http({
+			method: 'GET',
+			url: '/session/logout',
+			data: {
+				username: this.loggedOutUsername,
+				password: this.loggedOutPassword
+			}
+		}).then(
+		function(response){
+				controller.loggedIn = false;
+		},
+		function(err){
+			console.log(err);
+		});
+	};
+
+	this.showForm = function(){
+		this.hideForm = false;
+	};
+	
+	this.getYogaPoses();
+
 }]);
