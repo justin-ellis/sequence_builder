5d57592b82b8832979a002fe4471560f3771b565
Jellis <venetianself@hotmail.com>
2017-09-15 12:26:27 -0500

    added mongoose dependencies to server.js

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const User = require('./models/user.js');
+const session = require('express-session');
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+app.use(session({
+	secret: "random secret string",
+	resave: false,
+	saveUninitialized: false
+}));
+
+const usersController = require('./controllers/user.js');
+app.use('/user', usersController);
+
+const sessionController = require('./controllers/session.js');
+app.use('/session', sessionController);
+
+mongoose.connection.once('open', ()=>{
+    console.log('mongo is running');
+});
 
 app.listen(PORT, ()=>{
 	console.log('listening on port ' + PORT);
