dc2a2a845f1af45b1112874da610fae3e2b534d0
Jellis <venetianself@hotmail.com>
2017-09-23 13:29:42 -0500

    success! only one poose is deleted with there are duplicate poses in a sequence

--- controllers/asana.js
+++ controllers/asana.js
@@ -68,6 +68,9 @@ router.delete('/:id', (req, res)=>{
 		// foundSequence.poses[0].remove()
 		// console.log(foundSequence.poses.id(req.params.id) + "is thing with id");
 			let stringifiedIds = [];
+				let matchingPoses = [];
+				let matchingPosesIndex = [];
+				let shiftedIndex;
 			const requisite = req.params.id;
 			const newReq = '"'+requisite+'"';
 			console.log(newReq);
@@ -75,37 +78,53 @@ router.delete('/:id', (req, res)=>{
 			// console.log(foundSequence.poses[i]);
 				stringifiedIds.push(JSON.stringify(foundSequence.poses[i]._id));
 	// console.log(id + ' is id');
-			console.log(typeof req.params.id + ' is req.params.id');
-			// console.log(typeof foundSequence.poses[i]._id + ' is id of index' + i);
+			// console.log(typeof req.params.id + ' is req.params.id');
+			console.log(foundSequence.poses[i]._id + ' is id of index' + i);
 			for (var j = 0;  j <= stringifiedIds.length-1; j++) {
 			if (newReq === stringifiedIds[j]){
 				console.log('true');
-				console.log(j + " is the index number of the pose sequence poes index");
-				let matchingPoses = [];
-				matchingPoses.push(foundSequence.poses[i]);
+				console.log(j + " is the index number of the pose sequence pose index");
+				matchingPoses.push(foundSequence.poses[j]);
+				matchingPosesIndex.push(j);
+				console.log(matchingPosesIndex + ' is matching poses index');
 				if (matchingPoses.length > 1) {
-
+					console.log('DUPLICATES FOUND');
+					console.log('============================');
+					// console.log(foundSequence.poses[j] + " is poses of j");
+					// const shiftedIndex = matchingPosesIndex.shift();
+					// console.log(typeof shiftedIndex + " is shifted index");
 					// figure out which index array the matching poses
 					// are at in the origin found.sequence.poses[i]
 
 					//delete the correct data (only one pose)
 				}
-				console.log(matchingPoses + "are matching poses");
+				// console.log(matchingPoses + "are matching poses");
 			} else {
-				console.log('false');
+				console.log('false yo');
 				console.log(req.params.id + ' is req.params.id');
-				console.log(stringifiedIds[0] + ' is stringifiedIds[0]');
-				console.log(stringifiedIds[1] + ' is stringifiedIds[1]');
+				// console.log(stringifiedIds[0] + ' is stringifiedIds[0]');
+				// console.log(stringifiedIds[1] + ' is stringifiedIds[1]');
 			}
 			}
 				console.log(stringifiedIds + ' are stringifiedIds');
 		}
+					if (matchingPosesIndex.length > 1 && matchingPosesIndex[matchingPosesIndex.length-1] != matchingPosesIndex[matchingPosesIndex.length-2]) {
+						const shiftedIndex = matchingPosesIndex.pop();
+					console.log(shiftedIndex + " is shifted index");
+					foundSequence.poses.splice(shiftedIndex, 1);
+					foundSequence.save((err, savedSequence)=>{
+		});
+					} else {
+
+		// console.log(butts + ' is the butt');
 		// console.log(typeof foundSequence.poses.id(req.params.id) + "is thing with id");
 		// const butt = foundSequence.poses.id(req.params.id);
 		// console.log(butt);
 		foundSequence.poses.id(req.params.id).remove();
 		foundSequence.save((err, savedSequence)=>{
 		});
+					}
+
 		 res.json(deletedAsana);
 		 // res.json(foundSequence);
 			// console.log(foundSequence);
